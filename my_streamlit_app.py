85953129c649e86e43e9ea38a184634bcaacd9bf
AnnBoRo <124368475+AnnBoRo@users.noreply.github.com>
2024-04-20 17:51:13 +0200

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -23,8 +23,17 @@ import seaborn as sns
 st.title('Hello Wilders, welcome to my application!')
 st.write("I enjoy to discover streamlit possibilities")
 
+st.subheader('This is a subheader with a divider', divider='rainbow')
+st.subheader('_Streamlit_ is :blue[cool] :sunglasses:')
 
 link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+
+option = st.selectbox(
+   "Origine des voitures",
+   ("US", "Europe", "Japan"),
+   index=None,
+   placeholder="Select contact method...",
+)
 
 df_cars = pd.read_csv(link)
 
@@ -33,11 +42,5 @@ st.write(df_cars)
 # Here we use "magic commands":
 df_cars
 
-option = st.selectbox(
-   "Origine des voitures",
-   ("US", "Europe", "Japan"),
-   index=None,
-   placeholder="Select contact method...",
-)
 
 
