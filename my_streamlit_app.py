c6d61a7be7a04398fe28a820821a1d61cf672903
AnnBoRo <124368475+AnnBoRo@users.noreply.github.com>
2024-04-20 20:41:26 +0200

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -21,14 +21,29 @@ link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
 df_cars = pd.read_csv(link)
 
 # Ecriture des entetes de la page WEB Application
-st.title('VOITURE : USA vs EUROPE vs JAPON')
-st.subheader('Modele de voiture de 1971 à 1983', divider='rainbow')
+st.title('VOITURES DES ANNEES 1971 - 1983 ')
+st.subheader('USA / EUROPE / JAPON', divider='rainbow')
 st.write("AG BONNET - 04/2024")
 
+# Mettre du texte explicatif : 
+st.write('Présentation des donnnées cars.csv :')
+
+st.write('Le dataset fournit des spécifications techniques sur les modèles de voitures produits sur la période de 1971 à 1983.')
+st.write('Description des variables :)
+
+st.write('mpg (miles per gallon) : la consommation d'essence d'une voiture')
+st.write('cylinders: le nombre de cylindres du moteur')
+st.write('displacement: le volume interne du moteur à combustion (souvent en cm3)')
+st.write('horsepower: la puissance du moteur en chevaux')
+st.write('weight: le poids de la voiture (en pounds)')
+st.write('acceleration: la durée en seconde pour passer de l'arrêt (0 km/h) à 60 mph (environ 97 km/h)')
+st.write('year: l'année de production')
+st.write('continent : région de production')
+    
 
 
 option = st.selectbox(
-   "Visualisation par origine",
+   "Visualisation par continent",
    ("US", "Europe", "Japon"),
    index=None,
    placeholder="choisir une origine...",
